19084a6447a5c6d5659c51389da345944f7e241e
Mike Weaver <michael@triplewhale.com>
2023-01-25 15:46:41 -0500

    Update start/production build

--- examples/node/server.ts
+++ examples/node/server.ts
@@ -344,7 +344,7 @@ app.get("/logged-in", (req: Request, res: Response) => {
 })
 
 ViteExpress.listen(app, port, () => {
-  console.log(appName + chalk.green(`🐳🐳🐳 listening http://localhost:${NODE_ENV === 'production' ? '80' : port}`))
+  console.log(appName + chalk.green(`🐳🐳🐳 listening http://localhost:${port}}`))
   console.log(
     appName + (
       !!CLIENT_ID 
