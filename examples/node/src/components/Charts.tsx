e945d51203c7fb9fd8e2dd01a0814bd4eb20f0d0
Mike Weaver <michael@triplewhale.com>
2023-04-12 17:04:09 -0400

    Fix total/hover on donuts after new version

--- examples/node/src/components/Charts.tsx
+++ examples/node/src/components/Charts.tsx
@@ -1,13 +1,81 @@
 import { DonutChart, SparkLineChart, LineChart } from '@shopify/polaris-viz'
 import { annotationsData, sparkChartData } from '../types/Types'
 
+const numberStyles = {
+  fontWeight: 600,
+}
+
+const renderInnerValueContent = (values: any) => {
+  const { activeValue = 0, totalValue = 0 } = values
+  const activeValuePercentage = activeValue
+    ? `${((activeValue / totalValue) * 100).toFixed(1)}%`
+    : null
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: 'center',
+      }}
+    >
+      {activeValuePercentage && (
+        <p
+          style={{
+            fontSize: 20,
+            margin: 0,
+            ...numberStyles,
+          }}
+        >
+          {activeValuePercentage}
+        </p>
+      )}
+      <p
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: 4,
+        }}
+      >
+        {activeValue ? (
+          <span
+            style={{
+              fontSize: 15,
+              margin: 0,
+              ...numberStyles,
+            }}
+          >{`${activeValue} of ${totalValue}`}</span>
+        ) : (
+          <>
+            <span>Total:</span>
+            <p
+              style={{
+                fontSize: 20,
+                ...numberStyles,
+              }}
+            >
+              {totalValue}
+            </p>
+          </>
+        )}
+      </p>
+    </div>
+  )
+}
+
 export const DonutPieChart: React.FC<{ data: sparkChartData }> = (props: {
   data: sparkChartData
 }) => {
   const { data } = props
   return (
     <div className="donut-wrapper">
-      <DonutChart data={data as any} legendPosition="left" theme="Light" />
+      <DonutChart
+        data={data as any}
+        legendPosition="left"
+        theme="Light"
+        renderInnerValueContent={renderInnerValueContent}
+      />
     </div>
   )
 }
